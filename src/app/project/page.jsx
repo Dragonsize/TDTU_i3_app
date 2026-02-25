29bbc1cb2c860e248c221ce37eb838c5df8e87db
Dragonsize <tranphate@gmail.com>
2026-02-25 20:29:32 +0700

    Create page.jsx

---
+++ src/app/project/page.jsx
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+
+export default function ProjectPage() {
+  return (
+    <div className="min-h-screen bg-white flex flex-col items-center justify-center">
+      <div className="text-3xl font-bold text-gray-400">Project page placeholder</div>
+      <div className="mt-4 text-lg text-gray-500">This page will display your projects soon.</div>
+    </div>
+  );
+}
